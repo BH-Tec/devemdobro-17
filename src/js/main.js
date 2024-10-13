86eb60e0cfa7c171c8fee7a802e90f4c895f69ea
Bruno <brunodorea@outlook.com.br>
2024-10-12 21:52:47 -0300

    :refactor: refatorando js

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,32 @@ const listaSelecaoPokedevs = document.querySelectorAll('.pokedev')
 
 listaSelecaoPokedevs.forEach(pokedev => {
     pokedev.addEventListener('click', () => {
-        const cartaoPokedevAberto = document.querySelector('.aberto')        
-        cartaoPokedevAberto.classList.remove('aberto')
-
-        const idPokedevSelecionado = pokedev.attributes.id.value
-
-        const idDoCartaoPokedevParaAbrir = 'cartao-' + idPokedevSelecionado
-        const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir)
-        cartaoPokedevParaAbrir.classList.add('aberto')
-
-        const pokedevAtivoNaListagem = document.querySelector('.ativo')
-        pokedevAtivoNaListagem.classList.remove('ativo')
-
-        const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado)
-        pokedevSelecionadoNaListagem.classList.add('ativo')
+        esconderCartaoPokedev()
+        const idPokedevSelecionado = exibirCartaoPokedev(pokedev)
+        desativarPokedevNaListagem()
+        ativarPokedevNaListagem(idPokedevSelecionado)
     })
 })
+
+function ativarPokedevNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado)
+    pokedevSelecionadoNaListagem.classList.add('ativo')
+}
+
+function desativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector('.ativo')
+    pokedevAtivoNaListagem.classList.remove('ativo')
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector('.aberto')
+    cartaoPokedevAberto.classList.remove('aberto')
+}
+
+function exibirCartaoPokedev(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value
+    const idDoCartaoPokedevParaAbrir = 'cartao-' + idPokedevSelecionado
+    const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir)
+    cartaoPokedevParaAbrir.classList.add('aberto')
+    return idPokedevSelecionado
+}
